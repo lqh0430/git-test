419112721a29cbcd475754093be36db64296f2f0
lqh <1144708758@qq.com>
2022-09-02 11:49:46 +0800

    提交GitTest23

---
+++ src/main/java/com/atguigu/test2/GitTest2.java
@@ -0,0 +1,13 @@
+package com.atguigu.test2;
+
+/**
+ * @version 1.0
+ * @Author LQH02
+ * @Description
+ * @CreateDate 2022/9/2 11:47
+ */
+public class GitTest2 {
+    public static void main(String[] args) {
+        System.out.println("测试git");
+    }
+}
---
+++ src/main/java/com/atguigu/test2/GitTest3.java
@@ -0,0 +1,10 @@
+package com.atguigu.test2;
+
+/**
+ * @version 1.0
+ * @Author LQH02
+ * @Description
+ * @CreateDate 2022/9/2 11:48
+ */
+public class GitTest3 {
+}
