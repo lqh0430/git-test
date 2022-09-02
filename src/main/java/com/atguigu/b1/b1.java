24550a7473cac4f9439b7c688cb975d156460fc7
lqh <1144708758@qq.com>
2022-09-02 12:01:30 +0800

    创建b1分支

---
+++ src/main/java/com/atguigu/b1/b1.java
@@ -0,0 +1,14 @@
+package com.atguigu.b1;
+
+/**
+ * @version 1.0
+ * @Author LQH02
+ * @Description
+ * @CreateDate 2022/9/2 11:59
+ */
+public class b1 {
+    public static void main(String[] args) {
+        System.out.println("这是b1分支");
+        System.out.println("gittest5");
+    }
+}
