e423967b2584afe8a54b39beef5f437b5a10f87c
lqh <1144708758@qq.com>
2022-09-02 11:58:07 +0800

    提交GitTest4

---
+++ src/main/java/com/atguigu/test2/GitTest4.java
@@ -0,0 +1,13 @@
+package com.atguigu.test2;
+
+/**
+ * @version 1.0
+ * @Author LQH02
+ * @Description
+ * @CreateDate 2022/9/2 11:57
+ */
+public class GitTest4 {
+    public static void main(String[] args) {
+        System.out.println("这是git4");
+    }
+}
